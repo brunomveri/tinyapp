85fcde69a24a3e7a2632a5dd0cf0edddc99e3f25
vagrant <mveri.bruno@gmail.com>
2021-01-19 22:06:26 +0000

    Added delete buttom

--- express_server.js
+++ express_server.js
@@ -5,14 +5,22 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-function generateShortURL(length) {
+const generateShortURL = function(length) {
   return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
-}
+};
+
+const deleteData = function(shortURL) { //function to remove existing shortened URLs from our database
+  delete urlDatabase[shortURL];
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -33,6 +41,13 @@ app.post("/urls", (req, res) => {
 
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => { //implement a DELETE operation using POST
+  deleteData(req.params.shortURL);
+  const templateVars = { urls: urlDatabase };
+  // urlDatabase.deleteData(req.params.shortURL);
+  res.render('urls_index', templateVars);
+});
+
 app.get("/u/:shortURL", (req, res) => { //como surgiu a route /u/ ?
    const longURL = urlDatabase[req.params.shortURL];
    res.redirect(longURL);
@@ -43,9 +58,7 @@ app.get("/urls/:shortURL", (req, res) => { //para que serve o parametro ':shortU
   res.render("urls_show", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
