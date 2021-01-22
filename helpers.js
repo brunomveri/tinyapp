71c25221390ec1d5939820ce78f8ce8962898e6a
vagrant <mveri.bruno@gmail.com>
2021-01-22 19:47:48 +0000

    Created this file to store the getUserByEmail function and improve modularity on the project

---
+++ helpers.js
@@ -0,0 +1,15 @@
+const getUserByEmail = function(email, database) {
+  for (let user in database) {
+    if (email === database[user].email)
+    return database[user];
+  }
+  // return false;
+  return undefined;
+
+};
+
+
+
+module.exports = {
+  getUserByEmail,
+};
